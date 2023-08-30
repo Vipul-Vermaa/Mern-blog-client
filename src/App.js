6a9af3e89322b4f914b583494f11339a021ab931
Vipul <vipulvermalm8@gmail.com>
2023-08-30 10:17:57 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Blogs from './pages/Blogs';
+import UserBlog from './pages/UserBlog';
+import CreateBlog from './pages/CreateBlog';
+import BlogDetails from './pages/BlogDetails';
+import {Toaster} from 'react-hot-toast'
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <Router>
+       <Header/>
+       <Toaster/>
+       <Routes>
+        <Route path='/' element={<Blogs/>}/>
+        <Route path='/blogs' element={<Blogs/>}/>
+        <Route path='/my-blogs' element={<UserBlog/>}/>
+        <Route path='/blog-details/:id' element={<BlogDetails/>}/>
+        <Route path='/create-blog' element={<CreateBlog/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+       </Routes>
+    </Router>
+    
   );
 }
 
